4ac940222680ad189f6e75325db447b4fdca8c79
Karine Berth <karine.berth@gmail.com>
2017-07-01 23:23:34 +0200

    layout home-view

--- src/app/views/home-view/home-view.component.ts
+++ src/app/views/home-view/home-view.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Day } from "../../kalendar/day/day";
 
 @Component({
   selector: 'home-view',
@@ -8,25 +7,9 @@ import { Day } from "../../kalendar/day/day";
 })
 export class HomeViewComponent implements OnInit {
 
-  private day1: Day;  
-  private day2: Day;  
-  private day3: Day;  
-  private day4: Day;  
-
   constructor() { }
 
   ngOnInit() {
-    let today:Date = new Date();
-    let dayDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1, 12, 0, 0);
-    this.day1 = new Day(dayDate, today);
-    this.day2 = new Day(today, today);
-    dayDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1, 12, 0, 0);
-    this.day3 = new Day(dayDate, today);
-    dayDate = new Date(dayDate.getFullYear(), dayDate.getMonth(), dayDate.getDate() + 1, 12, 0, 0);
-    this.day4 = new Day(dayDate, today);
-    dayDate = new Date(dayDate.getFullYear(), dayDate.getMonth(), dayDate.getDate() + 1, 12, 0, 0);
-  }
-
-
+   }
 
 }
