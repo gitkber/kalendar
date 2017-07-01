6b9b887399db415c9677ef95d18d5ee891203cb5
Karine Berth <karine.berth@gmail.com>
2017-07-01 22:04:47 +0200

    Begin days-container

--- src/app/views/home-view/home-view.component.ts
+++ src/app/views/home-view/home-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Day } from "../../kalendar/day/day";
 
 @Component({
   selector: 'home-view',
@@ -8,9 +9,23 @@ import { Router } from '@angular/router';
 })
 export class HomeViewComponent implements OnInit {
 
+  private day1: Day;  
+  private day2: Day;  
+  private day3: Day;  
+  private day4: Day;  
+
   constructor(private router: Router) { }
 
   ngOnInit() {
+    let today:Date = new Date();
+    let dayDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1, 12, 0, 0);
+    this.day1 = new Day(dayDate, today);
+    this.day2 = new Day(today, today);
+    dayDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1, 12, 0, 0);
+    this.day3 = new Day(dayDate, today);
+    dayDate = new Date(dayDate.getFullYear(), dayDate.getMonth(), dayDate.getDate() + 1, 12, 0, 0);
+    this.day4 = new Day(dayDate, today);
+    dayDate = new Date(dayDate.getFullYear(), dayDate.getMonth(), dayDate.getDate() + 1, 12, 0, 0);
   }
 
   navigateToKalYear() {
