732845f5101b7b560766a7f922aba90c2641dead
gitkber <karine.berth@gmail.com>
2017-07-18 12:52:35 +0200

    css test flexbox

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { KalButtonComponent } from './kal-button/kal-button.component';
+import { KalLogoComponent } from './kal-logo/kal-logo.component';
 
 @NgModule({
     imports: [BrowserModule, RouterModule],
-    declarations: [KalButtonComponent],
-    exports: [KalButtonComponent],
+    declarations: [KalButtonComponent, KalLogoComponent],
+    exports: [KalButtonComponent, KalLogoComponent],
     providers: []
 })
 export class CommonModule {
---
+++ src/app/common/kal-logo/kal-logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kal-logo',
+  templateUrl: './kal-logo.component.html',
+  styleUrls: ['./kal-logo.component.css']
+})
+export class KalLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
