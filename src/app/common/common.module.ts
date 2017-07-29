1175f43ec9dd4b1c93427c7996cafdd1cba54c64
Karine Berth <karine.berth@gmail.com>
2017-07-29 10:31:31 +0200

    Review location images

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -7,12 +7,15 @@ import { KalLogoComponent } from './kal-logo/kal-logo.component';
 import { KalPolaroidComponent } from './kal-polaroid/kal-polaroid.component';
 import { DateStringPipe } from './pipe/date-string.pipe';
 import { SvgLoginComponent } from "./svg/svg-login.component";
+import { SvgSaveComponent } from "./svg/svg-save.component";
+import { SvgDeleteComponent } from "./svg/svg-delete.component";
+
 @NgModule({
     imports: [BrowserModule, RouterModule],
-    declarations: [KalButtonActionComponent, KalButtonMenuComponent, KalLogoComponent, KalPolaroidComponent, DateStringPipe,
-        SvgLoginComponent],
+    declarations: [KalButtonActionComponent, KalButtonMenuComponent, KalLogoComponent, KalPolaroidComponent, DateStringPipe, 
+        SvgLoginComponent, SvgSaveComponent, SvgDeleteComponent],
     exports: [KalButtonActionComponent, KalButtonMenuComponent, KalLogoComponent, KalPolaroidComponent, DateStringPipe,
-        SvgLoginComponent],
+        SvgLoginComponent, SvgSaveComponent, SvgDeleteComponent],
     providers: []
 })
 export class CommonModule {
