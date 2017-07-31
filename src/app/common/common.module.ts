8d1981e905f0c40ae2ce969390dd18c34bd208ca
gitkber <karine.berth@gmail.com>
2017-07-31 10:48:24 +0200

    Add common component panel-form

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { KalButtonActionComponent } from './kal-button-action/kal-button-action.component';
 import { KalButtonMenuComponent } from './kal-button-menu/kal-button-menu.component';
 import { KalLogoComponent } from './kal-logo/kal-logo.component';
 import { KalPolaroidComponent } from './kal-polaroid/kal-polaroid.component';
 import { DateStringPipe } from './pipe/date-string.pipe';
-import { SvgLoginComponent } from "./svg/svg-login.component";
-import { SvgSaveComponent } from "./svg/svg-save.component";
-import { SvgDeleteComponent } from "./svg/svg-delete.component";
+import { SvgLoginComponent } from './svg/svg-login.component';
+import { SvgSaveComponent } from './svg/svg-save.component';
+import { SvgDeleteComponent } from './svg/svg-delete.component';
+import { KalPanelFormComponent } from './kal-panel-form/kal-panel-form.component';
 
 @NgModule({
     imports: [BrowserModule, RouterModule],
-    declarations: [KalButtonActionComponent, KalButtonMenuComponent, KalLogoComponent, KalPolaroidComponent, DateStringPipe, 
-        SvgLoginComponent, SvgSaveComponent, SvgDeleteComponent],
+    schemas:      [ NO_ERRORS_SCHEMA ],
+    declarations: [KalButtonActionComponent, KalButtonMenuComponent, KalLogoComponent, KalPolaroidComponent, DateStringPipe,
+        SvgLoginComponent, SvgSaveComponent, SvgDeleteComponent,
+        KalPanelFormComponent],
     exports: [KalButtonActionComponent, KalButtonMenuComponent, KalLogoComponent, KalPolaroidComponent, DateStringPipe,
-        SvgLoginComponent, SvgSaveComponent, SvgDeleteComponent],
+        SvgLoginComponent, SvgSaveComponent, SvgDeleteComponent,
+        KalPanelFormComponent],
     providers: []
 })
 export class CommonModule {
---
+++ src/app/common/kal-panel-form/kal-panel-form.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'kal-panel-form',
+    templateUrl: './kal-panel-form.component.html',
+    styleUrls: ['./kal-panel-form.component.css']
+})
+export class KalPanelFormComponent {
+
+    @Input() title: string;
+    @Input() errorMessage: string;
+
+}
