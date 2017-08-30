3b7a40c71a5573e0ad66cf1b15d76a737f745197
gitkber <karine.berth@gmail.com>
2017-08-30 14:11:16 +0200

    Continue ContactHoliday

--- src/app/views/memos-view/memos-view.component.ts
+++ src/app/views/memos-view/memos-view.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { MemoService } from '../../core/memo/memo.service';
 import { Memo } from '../../core/memo/memo';
+import { MemoCriteria } from '../../core/memo/memo-criteria';
+import { Action } from '../../core/action';
+import { ContactHolidayService } from '../../core/holiday/contact-holiday/contact-holiday.service';
+import { ContactHolidayAction } from '../../core/holiday/contact-holiday/contact-holiday-action';
+import { ContactHoliday } from '../../core/holiday/contact-holiday/contact-holiday';
 
 @Component({
     selector: 'memos-view',
@@ -12,7 +17,7 @@ export class MemosViewComponent implements OnInit {
 
     public memos: FirebaseListObservable<Memo[]>
 
-    constructor(private memoService: MemoService) { }
+    constructor(private memoService: MemoService, private contactHolidayService: ContactHolidayService) { }
 
     ngOnInit() {
         this.memos = this.memoService.getList();
@@ -20,6 +25,15 @@ export class MemosViewComponent implements OnInit {
 
     showMemo(event: Memo) {
         console.log('showMemo action', event);
+
+        const memoCriteria: MemoCriteria = new MemoCriteria(null, null, event['$key'], null, null);
+        memoCriteria.action = Action.DELETE;
+        this.memoService.doActionOnMemo(memoCriteria);
+
+        const holiday = new ContactHoliday(null, null, event.description, event.kalendarDate);
+        const contactHolidayAction: ContactHolidayAction = new ContactHolidayAction(Action.INSERT);
+        contactHolidayAction.holiday = holiday;
+        this.contactHolidayService.doActionOnContactHoliday(contactHolidayAction);
     }
 
 }
