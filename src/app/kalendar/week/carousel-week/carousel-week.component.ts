1f9fd05ff747b7dde6ff1cd1548136628d06a7d5
gitkber <karine.berth@gmail.com>
2017-10-20 11:57:41 +0200

    Coontinue Draft View

--- src/app/kalendar/week/carousel-week/carousel-week.component.ts
+++ src/app/kalendar/week/carousel-week/carousel-week.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, Output, Renderer2 } from '@angular/core';
 import { Day } from '../../day/day';
 import { Navigation } from '../../navigation';
 import { CarouselWeek } from './carousel-week';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
     selector: 'carousel-week',
@@ -16,7 +17,7 @@ export class CarouselWeekComponent {
     @Output() navigateClick: EventEmitter<Navigation> = new EventEmitter();
     public directory: string;
 
-    constructor() {
+    constructor(@Inject(DOCUMENT) renderer: Renderer2) {
         this.directory = 'todo';
     }
 
@@ -37,6 +38,43 @@ export class CarouselWeekComponent {
     navigate(event: Navigation) {
         this.navigateClick.emit(event);
     }
+
+    test() {
+        // this.navigateClick.emit(event);
+        document.getElementById('prevId').setAttribute('class', 'prevLeftSecond');
+
+
+        document.getElementById('prevId').setAttribute('class', 'prevLeftSecond');
+        document.getElementById('selectedId').setAttribute('class', 'prev');
+        document.getElementById('nextId').setAttribute('class', 'selected');
+        document.getElementById('nextRightSecondId').setAttribute('class', 'next');
+        document.getElementById('prevLeftSecondId').setAttribute('class', 'nextRightSecond');
+    }
+
+    // function moveToSelected(element) {
+    //     if (element == "next") {
+    //         var selected = $(".selected").next();
+    //     } else if (element == "prev") {
+    //         var selected = $(".selected").prev();
+    //     } else {
+    //         var selected = element;
+    //     }
+    //     var next = $(selected).next();
+    //     var prev = $(selected).prev();
+    //     var prevSecond = $(prev).prev();
+    //     var nextSecond = $(next).next();
+    //
+    //     $(selected).removeClass().addClass("selected");
+    //
+    //     $(prev).removeClass().addClass("prev");
+    //     $(next).removeClass().addClass("next");
+    //
+    //     $(nextSecond).removeClass().addClass("nextRightSecond");
+    //     $(prevSecond).removeClass().addClass("prevLeftSecond");
+    //
+    //     $(nextSecond).nextAll().removeClass().addClass('hideRight');
+    //     $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+    // }
 
     showDayDetail(day: Day) {
         this.showDayDetailClick.emit(day);
